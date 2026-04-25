a238ee6499532ec51c538bd932540314c1976301
Tiziri-k <kashitiziri@gmail.com>
2026-04-25 21:07:07 +0200

    commit weights after training

---
+++ main.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+
+model = YOLO('Models/best.pt')
+
+path = "input_video/crb_match.mp4"
+results = model.predict(path, save=True, stream=True)
+
+
+ball_detected = 0
+
+person_detected = 0
+
+for result in results:
+    for box in result.boxes:
+        cls_name = model.names[int(box.cls[0])]
+        if cls_name == 'sports ball':
+            ball_detected += 1
+        if cls_name == 'person':
+            person_detected += 1
+print('start')
+print(f"Frames avec joueurs : {person_detected}")
+print(f"Frames avec ballon  : {ball_detected}")
+print(f"Classes détectées   : {set(model.names[int(b.cls[0])] for r in model.predict(path, save=True, stream=True) for b in r.boxes)}")
